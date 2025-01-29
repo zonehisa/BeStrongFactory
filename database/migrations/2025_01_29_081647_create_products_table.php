c1b64a7bce8089f37560c5d858a8ceead553d579
zonehisa <nijipapazoneokun123@gmail.com>
2025-01-29 10:59:23 +0900

    製品モデル作成、ダッシュボードのイメージ作成

---
+++ database/migrations/2025_01_29_081647_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('stock')->default(0);
+            $table->integer('safety_stock');
+            $table->integer('lead_time');
+            $table->integer('price')->default(0);
+            $table->integer('min_order_quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,29 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="flex">
+        <div class="w-64 h-screen bg-gray-800 text-white">
+            <ul>
+                <li class="p-4 hover:bg-gray-700"><a href="#">内示管理</a></li>
+                <li class="p-4 hover:bg-gray-700"><a href="#">製品管理</a></li>
+                <li class="p-4 hover:bg-gray-700"><a href="#">ユーザー管理</a></li>
+                <li class="p-4 hover:bg-gray-700"><a href="#">設定</a></li>
+                <li class="p-4 hover:bg-gray-700"><a href="#">ログアウト</a></li>
+            </ul>
+        </div>
+        <div class="w-full">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        ここに在庫の情報や、注文の情報などを表示する
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-10">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        ここに在庫の情報や、注文の情報などを表示する
+                    </div>
                 </div>
             </div>
         </div>
